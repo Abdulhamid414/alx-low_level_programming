51509eb2ce0469e8dd80b22589f8629477188929
Abdulhamid414 <abdulhamidmabini@gmail.com>
2023-07-14 02:24:52 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always O (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like buildinga multilingual puzzle");
+	return (0);
+}
